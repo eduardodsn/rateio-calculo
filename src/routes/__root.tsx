002345016008d970a4d6820a06dfaa9b8e46fb4a
Carlos Eduardo <carlosedsb0110@gmail.com>
2026-08-26 23:38:32 -0300

    ajustes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -94,8 +94,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       {
         rel: "stylesheet",
         href: appCss,
-      },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      }
     ],
   }),
   shellComponent: RootShell,
